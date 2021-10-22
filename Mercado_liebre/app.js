eb9b77bca48a123e266da0a121a32c86bbbb3194
Sebadooo <sebastianhalvarez@hotmail.com>
2021-10-21 23:00:44 -0300

    ok

--- Mercado_liebre/app.js
+++ Mercado_liebre/app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const path = require("path");
 
+const port = process.env.PORT || 3000;
+
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath));
 
-app.listen(5000, () => {
+app.listen(port, () => {
     console.log("Servidor activado")
 });
 
